54d8b392539c40074ac0f99f29102d32720b0b93
danieldavidyan <davidyan3153@gmail.com>
2020-08-19 15:57:01 +0300

    amaRomach 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { CartListComponent } from './cart-list/cart-list.component';
+import { CartProductComponent } from './cart-product/cart-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    NavBarComponent,
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    CartListComponent,
+    CartProductComponent
   ],
   imports: [
     BrowserModule
